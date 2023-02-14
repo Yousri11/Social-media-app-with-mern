1834931f2639ea6d6f2e1a3f9c036ea99bb93074
Yousri11 <yousri.meftah12@gmail.com>
2023-02-14 13:25:19 +0100

    prepre login page and installing react

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+function App() {
+  const router=createBrowserRouter(
+    [
+      {
+        path:"/",
+        element : <div>hello</div>
+      }
+    ]
+  )
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
