7370eefba65d4a0ba7d6d3ac0ebf8dae7dbb5a23
Yousri11 <yousri.meftah12@gmail.com>
2023-02-23 13:10:39 +0100

    register form

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -8,33 +8,39 @@ const Register =()=>{
     const [bio,setBio]=useState('')
     const [birthdate,setBirthdate]=useState('')
     const [picture,setPicture]=useState('')
+
+
+    const Register=(e)=>{
+        e.preventDefault()
+        console.log("saha lmef " , firstname)
+    }
     return <div className='all'>
             <div className='register'>
                 <div className='imgs'>
                     <div className='img'></div>                    
                 </div>
-                <div className='content'>
+                <form className='content' onSubmit={Register}>
                     <h1>DARK SPACE</h1>
                     <p>dark space social media appication</p>
                     <div className='inputs' >
                         
-                        <input type="text"  placeholder="username" value={firstname} onchange={(e)=>setFirstname(e.target.value)} />
+                        <input type="text"  placeholder="username" value={firstname} onChange={(e)=>setFirstname(e.target.value)} />
                     </div>
                     <div className='inputs'>
                         
-                        <input type="text"  placeholder="lastname" value={lastname} onchange={(e)=>setLastname(e.target.value)} />
+                        <input type="text"  placeholder="lastname" value={lastname} onChange={(e)=>setLastname(e.target.value)} />
                     </div>
                     <div className='inputs'>
                        
-                        <input type="email"  placeholder="email" value={email} onchange={(e)=>setEmail(e.target.value)} />
+                        <input type="email"  placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
                     </div>
                     <div className='inputs'>
                        
-                        <input type="password"  placeholder="password" value={password} onchange={(e)=>setPassword(e.target.value)} />
+                        <input type="password"  placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
                     </div>
                     <div className='inputs'>
                         
-                        <textarea name="" placeholder="your bio" id="" cols="30" rows="10" value={bio} onchange={(e)=>setBio(e.target.value)}></textarea>
+                        <textarea name="" placeholder="your bio" id="" cols="30" rows="10" value={bio} onChange={(e)=>setBio(e.target.value)}></textarea>
                     </div>
                     <div className='inputs'>
                         
@@ -42,10 +48,10 @@ const Register =()=>{
                     </div>
                     <div className='inputs'>
                         
-                        <input type="date"  placeholder="" value={birthdate} onchange={(e)=>setBirthdate(e.target.value)} />
+                        <input type="date"  placeholder="" value={birthdate} onChange={(e)=>setBirthdate(e.target.value)} />
                     </div>
-                    <button className='btnsign'>sign in</button>
-                </div>
+                    <button className='btnsign' type='submit'>sign in</button>
+                </form>
             </div>
     </div>
 }
